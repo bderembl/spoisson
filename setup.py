5ed9c26f937efc3ddba13f7b47518f40987965f8
bderembl <bderemble@fsu.edu>
2017-01-11 10:48:49 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='spoisson',
+      version='0.1',
+      description='Sparse poisson solver',
+      url='http://github.com/bderembl/spoisson',
+      author='bderembl',
+      author_email='bderemble@fsu.edu',
+      license='GNU gplv3',
+      packages=['spoisson'],
+      zip_safe=False)
